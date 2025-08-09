6e663258a4afe3d848f9753ee53ca1320def3515
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2025-08-09 12:18:34 +0100

    retry

--- src/services/tes.service.ts
+++ src/services/tes.service.ts
@@ -16,10 +16,15 @@ import type { SignedMetaTransaction } from "@src/utils/metatx";
 import { EvmClientService } from "@src/services/core/evmClient.service";
 import { AxiosInstance } from "axios";
 import { catchService } from "@src/services/core/catch.service";
+import { backOff } from "exponential-backoff";
 
 const AUTH_TOKEN_ABI = parseAbiParameters(
   "address authAddress,bytes authSignature,address ownerAddress,bytes delegationSignature",
 );
+
+const backoffOptions = {
+  numOfAttempts: 4,
+};
 
 export class TesService {
   constructor(
@@ -72,7 +77,7 @@ export class TesService {
     });
   }
 
-  async getAuthToken(random: Hex) {
+  private async getAuthToken(random: Hex) {
     const token = encodeAbiParameters(AUTH_TOKEN_ABI, [
       this.viewAccountService.getViewAccount()!.address,
       await this.signChallenge(random),
@@ -98,80 +103,88 @@ export class TesService {
     }
   }
 
-  async getTrustedEncryptionToken() {
-    await this.manageAuth();
-    const { data } = await this.axios.get<{
-      tepk: Hex;
-    }>(`${this.tesUrl}/encryption/tepk`);
-    return data.tepk;
+  getTrustedEncryptionToken() {
+    return backOff(async () => {
+      await this.manageAuth();
+      const { data } = await this.axios.get<{
+        tepk: Hex;
+      }>(`${this.tesUrl}/encryption/tepk`);
+      return data.tepk;
+    }, backoffOptions);
   }
 
-  async decrypt(block: string, token: Address, poseidonHash: string) {
-    await this.manageAuth();
+  decrypt(block: string, token: Address, poseidonHash: string) {
+    return backOff(async () => {
+      await this.manageAuth();
 
-    const requestData = {
-      block,
-      token,
-      poseidonHash,
-    };
+      const requestData = {
+        block,
+        token,
+        poseidonHash,
+      };
 
-    const response = await this.axios.post<{
-      decryptedCommitment: ReturnType<typeof serializeCommitment>;
-    }>(`${this.tesUrl}/encryption/decrypt`, requestData, {
-      headers: {
-        "Content-Type": "application/json",
-        "x-custom-tes-csrf": this.csrf,
-      },
-      withCredentials: true,
-    });
+      const response = await this.axios.post<{
+        decryptedCommitment: ReturnType<typeof serializeCommitment>;
+      }>(`${this.tesUrl}/encryption/decrypt`, requestData, {
+        headers: {
+          "Content-Type": "application/json",
+          "x-custom-tes-csrf": this.csrf,
+        },
+        withCredentials: true,
+      });
 
-    return deSerializeCommitment(response.data.decryptedCommitment);
+      return deSerializeCommitment(response.data.decryptedCommitment);
+    }, backoffOptions);
   }
 
   async syncWithTes(token: Address, fromBlock: string, toBlock: string) {
     try {
-      await this.manageAuth();
+      const result = await backOff(async () => {
+        await this.manageAuth();
 
-      this.logger.log(`syncing with TES from ${fromBlock} to ${toBlock}`);
-      const { data } = await this.axios.get<{
-        events: {
-          eventName: "CommitmentCreated" | "CommitmentRemoved";
-          args: {
-            owner: string;
-            token: string;
-            poseidonHash: string;
-            metadata?: string;
-          };
-          blockNumber: string;
-          transactionIndex: number;
-          transactionHash: string;
-        }[];
-        syncedBlock: string;
-      }>(
-        `${this.tesUrl}/indexer?owner=${this.mainAccountAddress}&token=${token}&fromBlock=${fromBlock}&toBlock=${toBlock}`,
-        {
-          headers: {
-            "x-custom-tes-csrf": this.csrf,
+        this.logger.log(`syncing with TES from ${fromBlock} to ${toBlock}`);
+        const { data } = await this.axios.get<{
+          events: {
+            eventName: "CommitmentCreated" | "CommitmentRemoved";
+            args: {
+              owner: string;
+              token: string;
+              poseidonHash: string;
+              metadata?: string;
+            };
+            blockNumber: string;
+            transactionIndex: number;
+            transactionHash: string;
+          }[];
+          syncedBlock: string;
+        }>(
+          `${this.tesUrl}/indexer?owner=${this.mainAccountAddress}&token=${token}&fromBlock=${fromBlock}&toBlock=${toBlock}`,
+          {
+            headers: {
+              "x-custom-tes-csrf": this.csrf,
+            },
+            withCredentials: true,
           },
-          withCredentials: true,
-        },
-      );
+        );
 
-      return {
-        syncedBlock: data.syncedBlock,
-        events: data.events.map((event) => ({
-          eventName: event.eventName,
-          args: {
-            ...event.args,
-            poseidonHash: BigInt(event.args.poseidonHash),
-          },
-          blockNumber: BigInt(event.blockNumber),
-          transactionIndex: event.transactionIndex,
-          transactionHash: event.transactionHash as Hash,
-        })),
-      };
-    } catch (error) {
-      catchService.catch(error as Error);
+        return {
+          syncedBlock: data.syncedBlock,
+          events: data.events.map((event) => ({
+            eventName: event.eventName,
+            args: {
+              ...event.args,
+              poseidonHash: BigInt(event.args.poseidonHash),
+            },
+            blockNumber: BigInt(event.blockNumber),
+            transactionIndex: event.transactionIndex,
+            transactionHash: event.transactionHash as Hash,
+          })),
+        };
+      }, backoffOptions);
+
+      return result;
+    } catch {
+      catchService.catch(new Error("Sync with TES failed"));
       return {
         syncedBlock: fromBlock,
         events: [],
@@ -180,42 +193,46 @@ export class TesService {
   }
 
   async quote(token: Address) {
-    await this.manageAuth();
-    const { data } = await this.axios.get<{
-      gasPrice: string;
-      paymasterAddress: Address;
-    }>(`${this.tesUrl}/paymaster/quote/${token}`, {
-      headers: {
-        "x-custom-tes-csrf": this.csrf,
-      },
-      withCredentials: true,
-    });
+    return backOff(async () => {
+      await this.manageAuth();
+      const { data } = await this.axios.get<{
+        gasPrice: string;
+        paymasterAddress: Address;
+      }>(`${this.tesUrl}/paymaster/quote/${token}`, {
+        headers: {
+          "x-custom-tes-csrf": this.csrf,
+        },
+        withCredentials: true,
+      });
 
-    return {
-      gasPrice: BigInt(data.gasPrice),
-      paymasterAddress: data.paymasterAddress,
-    };
+      return {
+        gasPrice: BigInt(data.gasPrice),
+        paymasterAddress: data.paymasterAddress,
+      };
+    }, backoffOptions);
   }
 
   async executeMetaTransaction(
     metatx: SignedMetaTransaction,
     coveredGas: string,
   ) {
-    await this.manageAuth();
-    await this.axios.post(
-      `${this.tesUrl}/paymaster/execute`,
-      {
-        metatx,
-        coveredGas,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "x-custom-tes-csrf": this.csrf,
+    return backOff(async () => {
+      await this.manageAuth();
+      await this.axios.post(
+        `${this.tesUrl}/paymaster/execute`,
+        {
+          metatx,
+          coveredGas,
         },
-        withCredentials: true,
-      },
-    );
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-custom-tes-csrf": this.csrf,
+          },
+          withCredentials: true,
+        },
+      );
+    }, backoffOptions);
   }
 
   reset() {
