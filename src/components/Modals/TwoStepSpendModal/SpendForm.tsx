5360d0602e5478e9aae427a4cffb09250f0dfe33
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2025-09-04 14:58:01 +0100

    refactor

--- src/components/Modals/TwoStepSpendModal/SpendForm.tsx
+++ src/components/Modals/TwoStepSpendModal/SpendForm.tsx
@@ -16,6 +16,7 @@ import {
 } from "@src/components/Panel/hooks/useFees";
 import { LedgerContext } from "@src/context/ledger/ledger.context";
 import { QRScannerModal } from "./QRScannerModal";
+import { CameraIcon } from "@src/components/svg/CameraIcon";
 
 const amountRegex = /^\d*\.?\d*$/;
 
@@ -160,25 +161,7 @@ export const SpendForm = ({
             className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-white transition-colors"
             title="Scan QR code"
           >
-            <svg
-              className="w-5 h-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
+            <CameraIcon />
           </button>
         </div>
         <div className="h-6 mt-1 text-base text-red-400">
