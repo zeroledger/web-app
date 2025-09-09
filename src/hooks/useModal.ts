caa0ea237fee7d52227a033dba8abd1a7e4034b9
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2025-09-09 11:49:47 +0100

    linting

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,23 +1,12 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import { useModalBehavior } from "./useModalBehavior";
 
 export const useModal = (options?: Parameters<typeof useModalBehavior>[2]) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const openModal = useCallback(() => {
-    setIsOpen(true);
-  }, []);
-
-  const closeModal = useCallback(() => {
-    setIsOpen(false);
-  }, []);
-
   const modalBehavior = useModalBehavior(isOpen, setIsOpen, options);
 
   return {
-    isOpen,
-    openModal,
-    closeModal,
     ...modalBehavior,
   };
 };
